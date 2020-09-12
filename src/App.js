976a3a42a5e0d7e249ef52d0a9ab07524c6567f3
Alexander Hoinville <ahoin001@gmail.com>
2020-09-11 23:03:32 -0400

    Attempt #1 To fix deployment

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
           const apiResponseObjectWithArrayOfCats = await response.json()
 
           console.log('Response from cat list API Route: ', apiResponseObjectWithArrayOfCats.cats)
-
+          
           const catList = apiResponseObjectWithArrayOfCats.cats;
 
           let randomCatIndex = Math.floor(Math.random() * (catList.length - 1) + 1);
